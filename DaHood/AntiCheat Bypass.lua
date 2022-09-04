1f84bb7ab7c70fd8894b0dcdd8111461ec0f87eb
Super <sondre.alexander.navarsete@gmail.com>
2022-09-04 15:50:26 +0200

    Create AntiCheat Bypass.lua
    Anti-Cheat bypass to prevent Da Hood from banning you

---
+++ DaHood/AntiCheat Bypass.lua
@@ -0,0 +1,37 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local dahood_id = {2788229376, 7213786345} -- Place Id for Da Hood normal and vc
+local GetFullName = game.GetFullName
+
+local function debug_print(...)
+    --printconsole(table.concat({...}), 255, 115, 115)
+end
+
+if not table.find(dahood_id, game.PlaceId) then return end
+
+printconsole(game.Players.LocalPlayer.Name .. " has joined DaHood! Anti-Cheat Running", 255, 255, 0)
+local anticheat_flags = {"TeleportDetect", "CHECKER_1", "CHECKER_2", "OneMoreTime", "BreathingHAMON"} -- Flags that trigger anticheat
+local Tostring = tostring   
+
+local oldNamecall = nil
+oldNamecall = hookmetamethod(game, "__namecall", function(...)
+    if getnamecallmethod() == "FireServer" then
+        local self, arg1 = ...
+
+        if table.find(anticheat_flags, arg1) then
+            debug_print("DETECTED FROM '" .. GetFullName(getcallingscript()) .. "': " .. Tostring(arg1), 255, 115,115)
+            return false
+        end
+    end
+
+    if getfenv(2).crash then -- From Stefanuk12's github
+        -- // Hook the crash function to prevent game from crashing
+        hookfunction(getfenv(2).crash, function()
+            debug_print(GetFullName(getcallingscript()) .. ": Crash Attempt")
+        end)
+    end
+    
+    return oldNamecall(...)
+end)
